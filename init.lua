83dced88102db81ae6d6d9d2ee9e4fbf987ff674
Gustavo Cosme <gcosme@uber.com>
2023-10-06 18:13:14 -0300

    Change theme to kanagawa

--- init.lua
+++ init.lua
@@ -143,11 +143,11 @@ require('lazy').setup({
   },
 
   {
-    -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
+    -- Theme
+    'rebelot/kanagawa.nvim',
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'onedark'
+      vim.cmd.colorscheme 'kanagawa'
     end,
   },
 
